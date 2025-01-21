9536d8c8a75f3574d35df20b882d93cc11ce0324
Jim <jimsamuel50@gmail.com>
2025-01-21 11:45:53 +0100

    LAlgb : Ensure slope EC values are value contained parameters

--- curves/curves.c
+++ curves/curves.c
@@ -12,12 +12,14 @@ uint64_t slope(uint64_t y2, uint64_t y1, uint64_t x2, uint64_t x1)
 
 uint64_t slopevalx(uint64_t slope, uint64_t x1, uint64_t x2)
 {
+    if (slope == NULL || slope == 0 && x1 == 0 || x2 == 0) return;
     uint64_t slopex3 = slope - x1 - x2;
     return slopex3;
 }
 
 uint64_t slopevaly(uint64_t slope, uint64_t x1, uint64_t x3, uint64_t y1)
 {
+    if (x1 == NULL || x3 == NULL || y1 == NULL) return;
     uint64_t slopey3 = slope * (x1 - x3) - y1;
     return slopey3;    
 }
